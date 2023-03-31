e608534c2d31cad86d9bb3e715401b39ea2e78d0
mehedi-hasan <mdmehedihasan2810@gmail.com>
2023-03-31 15:22:28 +0600

    succesfully fetched data and maped

--- src/components/Blogs/Blogs.jsx
+++ src/components/Blogs/Blogs.jsx
@@ -1,10 +1,30 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./Blogs.scss";
 import Blog from "../Blog/Blog";
 const Blogs = () => {
+const [blogs, setBlogs] = useState([])
+
+
+  useEffect(() => {
+    const abortController = new AbortController();
+    fetch("src/fakeData/blogs.json", {
+      signal: abortController.signal,
+    })
+      .then((res) => res.json())
+      .then((data) => setBlogs(data));
+
+    return () => {
+      abortController.abort();
+    };
+  }, []);
+
   return (
-    <div>
-      <Blog />
+    <div className="blogs-container">
+      {
+        blogs.map((blog) => (
+          <Blog key={blog.id} {...blog} />
+        ))
+      }
     </div>
   );
 };
